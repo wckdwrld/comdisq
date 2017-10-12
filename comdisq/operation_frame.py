8570660d6be24e2e51b1719db7dc0d788cc58cd8
Jack Bennett <z3484290@zmail.unsw.edu.au>
2017-10-13 09:44:29 +1100

    Added Operation Frame
    Op Frame will help communicate across threaded objects.

---
+++ comdisq/operation_frame.py
@@ -0,0 +1,30 @@
+import sys
+import os
+from routeing import User
+
+
+class OpFrame:
+
+    def __init__(self):
+        self.op_code = 0
+        self.context = ""
+        self.contact = User()
+
+    def getOpCode():
+        return self.op_code
+
+    def setOpCode(code):
+        self.op_code = code
+
+    def getContext():
+        return context
+
+    def setContext(word_list):
+        context = word_list
+
+    def getContact():
+        return contact
+
+    def setContact(user):
+        self.contact = user
+
